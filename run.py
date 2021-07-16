6250b3e9fb37e125ad8957f601e599d073a0884c
jieun1128 <5656jieun@naver.com>
2021-07-16 12:15:39 +0900

    feat : flask와 mysql 연동

--- run.py
+++ run.py
@@ -2,14 +2,21 @@
 from flask_cors import cross_origin
 from flask import Flask, render_template, request
 #from main import text_to_speech
-from inference import synthesize
+#from inference import synthesize
 from flask_ngrok import run_with_ngrok
-import IPython.display as ipd
-import soundfile as sf
+#import IPython.display as ipd
+#import soundfile as sf
+from saveText import save_text
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
-app = Flask(__name__,template_folder='/content/TTS') #html 폴더 경로 설정
-run_with_ngrok(app)
+app = Flask(__name__,template_folder='') #html 폴더 경로 설정
+app.config.from_pyfile('config.py')
+database = create_engine(app.config['DB_URL'], encoding = 'utf-8', max_overflow = 0)
+Session = sessionmaker(database)
+session = Session()
+#run_with_ngrok(app)
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -19,14 +26,15 @@ def homepage():
         text = request.form['speech']
         voice = request.form['voices']
         #text_to_speech(text,voice)
-        wavs=synthesize(text)
+        #wavs=synthesize(text)
         #ipd.display(ipd.Audio(wavs,rate=22050,autoplay=True))
-        sf.write('/content/audio.wav',wavs, 22050, 'PCM_24')
+        save_text(text,database,session)
+        #sf.write('/content/audio.wav',wavs, 22050, 'PCM_24')
         return render_template('frontend.html')
     else:
         return render_template('frontend.html')
 
 
 if __name__ == "__main__":
-    app.run()
-    #app.run(port=8000, debug=True)
+    #app.run('0.0.0.0',port = 8000)
+    app.run(port=8000, debug=True)
